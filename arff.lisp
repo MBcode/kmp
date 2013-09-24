206c4fc3be43b67cc65d0fdee5a88e633eb6fd25
mike b <mike.bobak+git@gmail.com>
2013-09-24 00:13:59 -0700

    v-simple arff km cls

--- arff.lisp
+++ arff.lisp
@@ -1,12 +1,15 @@
 (al 'cl-arff-parser)
 (use-package :cl-arff-parser)
 ;(pprint (parse-arff "example.arff"))
-(defvar *i* (parse-arff "example.arff")) 
+;(defvar *i* (parse-arff "example.arff")) 
+ 
 (defun get-arff (&optional (fn "example.arff")) 
   (let* ((ains (parse-arff fn))
          (cls (first (split-by #\. fn)))
          (attr (arff-attributes ains)))
+    ;(sv-cls cls Thing) ;but not as an is,as subclass
     (sv-cls fn cls)
-    (sv-al fn attr) ;but need2turn2alst 1st
+    (sv-al fn attr) ;but need2turn2alst 1st /so not so embedded
     ))
 
+(trace get-arff)
--- ld.cl
+++ ld.cl
@@ -1,8 +1,14 @@
-(lkm2) ;.sbclrc load of km&utils
+;play w/here before going to an asd file
+;(lkm2) ;.sbclrc load of km&utils
+(lkm3) ;lkm3 = lu+lkm2
 ;find my loadable alg3 http://www.cs.utexas.edu/users/qr/algy/download.html &try2work w/
 (ql 'cl-store) ;don't use the one that comes w/mlcl
 (al 'mlcl)
+(load "arff.lisp" :print t) ;cl-arff-parser to km
+(get-arff)
 
 ;need km components diretory for this:
 (load "load-lib.lisp" :print t)
-(load_lib)
+(load_lib) ;not working yet
+
+(taxonomy)
