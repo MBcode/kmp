db9f5130cdfcce0e82254db4dfb4b62343908953
mike b <mike.bobak+git@gmail.com>
2013-09-27 01:42:23 -0700

    start subprob inv..

--- owl2km.lisp
+++ owl2km.lisp
@@ -43,25 +43,45 @@
 (defun owl-oprop-p (l) (eq (first-lv l) '|owl|:|ObjectProperty|))
 (defun rdfs-dmn-p (lol) (lol-eq-p lol '|rdfs|:|domain|))
 (defun rdfs-rng-p (lol) (lol-eq-p lol '|rdfs|:|range|))
+(defun rdfs-inv-p (lol) (lol-eq-p lol '|owl|:|inverseOf|))
+(defun rdfs-sprop-p (lol) (lol-eq-p lol '|rdfs|:|subPropertyOf|))
+;add DatatypeProperty
+(defun g3v (l) (when (full l) (rm-pound (third-lv (caar l)))))
 
 (defun owl2km-prop (l)
   "s-xml lol w/class&super info"
   (let ((pp (collect-if #'owl-oprop-p l))
         (dp (collect-if #'rdfs-dmn-p l))
-        (rp (collect-if #'rdfs-rng-p l)))
-    (when (and pp dp rp)
+        (rp (collect-if #'rdfs-rng-p l))
+        (iv (collect-if #'rdfs-inv-p l))
+        (sp (collect-if #'rdfs-sprop-p l))
+        )
+    (when (and pp dp rp) ;think domain&ranger always given in these owl files
       (let ((prop (rm-pound (third-lv (first-lv pp))))
-            (dmn (third-lv (caar dp)))
-            (rng (third-lv (caar rp))))
+           ;(dmn (third-lv (caar dp)))
+           ;(rng (third-lv (caar rp)))
+            (dmn (g3v dp))
+            (rng (g3v rp))
+            (inv (g3v iv))
+            (sprop (g3v sp))
+            )
         (format t "~%prop:~a has domain:~a and range:~a" prop dmn rng) ;for now,then set w/km fnc
         ;(sn  has (instance-of (slot))  (domain (Thing)) (range (Thing)))
         ;(sv-cls sn "slot") (sv sn "domain" ?d) ..
         ;(sv-cls prop "slot")
         (sv- prop "instance-of" "slot")
-        (sv- prop "domain" (rm-pound dmn))
-        (sv- prop "range" (rm-pound rng))
+       ;(sv- prop "domain" (rm-pound dmn))
+       ;(sv- prop "range" (rm-pound rng))
+        (sv- prop "domain" dmn)
+        (sv- prop "range"  rng)
+        ;check these:
+        (when inv (sv- prop "inverseProperty"  inv))
+        (when sprop (sv- sprop "instance-of" prop))
 ))))
 ;will also need2capture heirarchical properties
+;((|owl|:|ObjectProperty| |rdf|:ID "headOf") (|rdfs|:|label| "is the head of")
+; ((|rdfs|:|subPropertyOf| |rdf|:|resource| "#worksFor"))) 
+ 
 (trace owl-oprop-p rdfs-dmn-p rdfs-rng-p owl2km-prop)
 ; decide if a lol is cls or property, &just call the proper owl2km-... fnc
 (defun owll2km (l)
