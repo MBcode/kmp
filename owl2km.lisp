62316a8752e069966266536e0926a84fd43e629a
mike b <mike.bobak+git@gmail.com>
2013-09-28 00:18:04 -0700

    assert Slot hier

--- owl2km.lisp
+++ owl2km.lisp
@@ -46,7 +46,10 @@
 (defun rdfs-inv-p (lol) (lol-eq-p lol '|owl|:|inverseOf|))
 (defun rdfs-sprop-p (lol) (lol-eq-p lol '|rdfs|:|subPropertyOf|))
 ;add DatatypeProperty
+(defun rdfs-dprop-p (lol) (lol-eq-p lol '|owl|:|DatatypeProperty|))
 (defun g3v (l) (when (full l) (rm-pound (third-lv (caar l)))))
+
+;even though each lol small, might be better2have1fnc that trys all -p instead of collecting
 
 (defun owl2km-prop (l)
   "s-xml lol w/class&super info"
@@ -55,6 +58,7 @@
         (rp (collect-if #'rdfs-rng-p l))
         (iv (collect-if #'rdfs-inv-p l))
         (sp (collect-if #'rdfs-sprop-p l))
+        (sd (collect-if #'rdfs-dprop-p l))
         )
     (when (and pp dp rp) ;think domain&ranger always given in these owl files
       (let ((prop (rm-pound (third-lv (first-lv pp))))
@@ -64,12 +68,13 @@
             (rng (g3v rp))
             (inv (g3v iv))
             (sprop (g3v sp))
+            (dprop (g3v sd))
             )
         (format t "~%prop:~a has domain:~a and range:~a" prop dmn rng) ;for now,then set w/km fnc
         ;(sn  has (instance-of (slot))  (domain (Thing)) (range (Thing)))
         ;(sv-cls sn "slot") (sv sn "domain" ?d) ..
         ;(sv-cls prop "slot")
-        (sv- prop "instance-of" "slot")
+        (sv- prop "instance-of" "Slot")
        ;(sv- prop "domain" (rm-pound dmn))
        ;(sv- prop "range" (rm-pound rng))
         (sv- prop "domain" dmn)
@@ -77,6 +82,7 @@
         ;check these:
         (when inv (sv- prop "inverseProperty"  inv))
         (when sprop (sv- sprop "instance-of" prop))
+        (when dprop (sv- sprop "instance-of" prop))
 ))))
 ;will also need2capture heirarchical properties
 ;((|owl|:|ObjectProperty| |rdf|:ID "headOf") (|rdfs|:|label| "is the head of")
