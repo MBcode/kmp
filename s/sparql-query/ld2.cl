4b3e401c45c0f8bb0e8cb3e60724e46d0773a6d2
MBcode <bobak@balisp.org>
2016-12-06 14:24:51 -0800

    cache qry response too

--- s/sparql-query/ld2.cl
+++ s/sparql-query/ld2.cl
@@ -1,3 +1,5 @@
 ;(load "tsparql.cl" :print t)
 (load "tsparql.lisp" :print t)
-(defvar *o2* (t2)) 
+;(defvar *o2* (t2))  ;make a version that can read the tmp file
+;(with-open-file (strm "o2.tmp" :direction :output :if-exists :supersede)
+;  (print *o2* strm))
--- s/sparql-query/tsparql.lisp
+++ s/sparql-query/tsparql.lisp
@@ -27,6 +27,7 @@
   (tshell-cmnd-sxml str)) 
 
 (defun tf (&optional (fn "i2.txt") (str *sq-f*))
+  "shell-cmd < fn"
   (tshell-cmnd-sxml (str-cat str fn)))
 
 ;now have a template query, &parts to change
@@ -35,7 +36,9 @@
 (defvar *tc* "select distinct ?C where { ?C rdf:type dbo:Organisation.  ?C rdfs:label ?title . ")
 (defvar *ffs* "FILTER REGEX(?title, \"~a\", \"i\")")
 ;}
+;fl==filter-list, used w/template to make a filtered query
 (defun tc (fl &optional (tc *tc*)) 
+  "create qry"
   ;loop for fs in fl collect  
   (when (full fl)
     (str-cat tc
@@ -43,17 +46,27 @@
            "}")
   ))
 (defun tcs (fl &optional (tc *tc*) (sf "t2.tmp")) 
+  "create qry, &cache"
   (save-lines (append (list tc) 
            (mapcar #'(lambda (f) (format nil *ffs* f)) fl)
               '(" } LIMIT 10"))  sf))
 (defun rtcs (fl)
+  "run cached qry"
   (tcs fl)
   (tf "i2.tmp"))
+
 (defun mk-tmp-fn (l) (str-cat "tmp/" (underscore_ (implode-l l)) ".tmp"))
+
+(defun print2tmpl (fn l)
+  (with-open-file (strm fn :direction :output :if-exists :supersede)
+      (print l strm)))
+
 (defun tsfl (fl)
+  "use|mk cached qry and run"
   (let ((tf (mk-tmp-fn fl)))
     (unless (file-exists-p tf) (tcs fl *tc* tf))
-    (tf tf)))
+    (tf tf)
+    ))
 (defun subseq-mx (s mx) (subseq s 0 (min mx (len s))))
 (defun subseq-mx2 (s) (subseq-mx s 2))
 (defun f2sq (fn) ;try: "c2o6.csv"
@@ -61,3 +74,19 @@
   ;let ((loqw (mapcar #'explode= (list-lines fn))))
   (let ((loqw (mapcar #'subseq-mx2 (mapcar #'explode= (list-lines fn)))))
     (mapcar #'tsfl loqw)))
+
+;I should cache the returns as well, so I don't have to hit dbpedia during testing/reuse
+ ;do as above or use a caching lib  ;might start w/just appending tf w/.out  or .l
+   ;just need strs for save-lines or similar, a sv-sexp(print 2file strm) so could load again right away/find
+ ;consider saving qry str in out file,(in case it changes/just4gen provenance)
+;defun tsfl (fl)
+(defun qfl (fl) ;qry orgs, w/filter list on parts of name
+  "use|mk cached qry and run, &cache"
+  (let* ((tf (mk-tmp-fn fl))
+         (tfl (str-cat tf ".l")))
+    (unless (file-exists-p tf) (tcs fl *tc* tf))
+    (if (file-exists-p tfl) (list-lines tfl) ;finish
+      (print2tmpl tfl (tf tf))) ;run-qry&cache
+    ));next print.. (append qry-l resp-l)
+(defun t2b (&optional (fl '("parc")))
+   (qfl fl))
